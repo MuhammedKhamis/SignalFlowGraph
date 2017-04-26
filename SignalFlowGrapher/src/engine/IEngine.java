b554b3eb986661f4656234dadb3fd602a7b1ad4e
MuhammedKhamis <muhammedessam10@gmail.com>
2017-04-26 22:55:26 +0200

    Interface Modifications

--- SignalFlowGrapher/src/engine/IEngine.java
+++ SignalFlowGrapher/src/engine/IEngine.java
@@ -1,20 +1,35 @@
 package engine;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+
 public interface IEngine {
-	
-	//ADD COMPONENET
-	public void addNode(String nodeName);
-	
-	public void addEdge(String fromNode, String toNode, double value);
-	
-	//MODIFY COMPONENT
-	public void modifyNodeName(String oldName, String newName);
-	
-	public void modifyEdgeValue(String startNode, String endNode, double newValue);
-	
-	//REMOVE COMPONENT
-	
-	//CALCULATE TRANSFER FUNCTION
-	public double calculateTFFunction(String fromNode, String toNode);		
+
+    // ADD COMPONENET
+    public void addNode(String nodeName);
+
+    public void addEdge(String fromNode, String toNode, double value);
+
+    // MODIFY COMPONENT
+    public void modifyNodeName(String oldName, String newName);
+
+    public void modifyEdgeValue(String startNode, String endNode, double newValue);
+
+    // REMOVE COMPONENT
+
+    // CALCULATE TRANSFER FUNCTION
+    public double calculateTFFunction(String fromNode, String toNode);
+
+    public ArrayList<ArrayList<String>> getForwardPaths();
+
+    public ArrayList<ArrayList<String>> getLoops();
+
+    public Hashtable<Integer, ArrayList<ArrayList<ArrayList<String>>>> getCombinations();
+
+    public double getDelta();
+
+    public ArrayList<Double> getDeltas();
+
+    public void clearCanvas();
 
 }
