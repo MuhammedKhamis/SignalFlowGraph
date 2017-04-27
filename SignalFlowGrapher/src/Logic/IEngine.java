a2032b588498ed4334558a7942968ace999ed1cc
MuhammedKhamis <muhammedessam10@gmail.com>
2017-04-27 21:41:41 +0200

    uml

--- SignalFlowGrapher/src/Logic/IEngine.java
+++ SignalFlowGrapher/src/Logic/IEngine.java
@@ -4,10 +4,12 @@ public interface IEngine {
 
     // ADD COMPONENET
     public void addNode(String nodeName);
+
     public void addEdge(String fromNode, String toNode, double value);
 
     // MODIFY COMPONENT
     public void modifyNodeName(String oldName, String newName);
+
     public void modifyEdgeValue(String startNode, String endNode, double newValue);
 
     // REMOVE COMPONENT
@@ -17,12 +19,11 @@ public interface IEngine {
     public double calculateTFFunction(String fromNode, String toNode);
 
     public String getForwardPaths();
+
     public String getIndividualLoops();
+
     public String getNonTouchingLoops();
+
     public String getDeltas();
-
-   
-
-    
 
 }
